f1f7829e1e377c757ad0904c990f5a41630b11f1
Anthony Garcia <anthony.garcia@digicatapult.org.uk>
2018-08-07 14:03:44 +0100

    Debug version working

--- README.md
+++ README.md
@@ -3,8 +3,31 @@
 This code run on a raspberry pi.
 It reads the coordinates from the UWB through serial port and store them in a remote PGSQL.
 
+## Installation
+
+This package require node 8+ and yarn:
+
+Node:
+
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+
+Yarn:
+
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt-get update && sudo apt-get install yarn
+
 ## TODO
 
 - Error handling (try to reconnect)
 - Add sentry
 - Enter shell mode etc
+
+## Hardware
+
+DWM1001 TWR Real Time Location System
+Copyright : 2016-2017 LEAPS and Decawave
+License : Please visit https://decawave.com/dwm1001_license
+Compiled : Nov 2 9 2017 13:35:02
+Help : ? or help
--- app/server.js
+++ app/server.js
@@ -1,6 +1,9 @@
 const Readline = require('@serialport/parser-readline')
 const Ready = require('@serialport/parser-ready')
 
+const { StringDecoder } = require('string_decoder')
+const decoder = new StringDecoder('utf8')
+
 const parseCoordinates = csv => {
   const [cmd, arg, sensor_id, x, y, z, accuracy, hex] = csv.split(',')
 
@@ -27,19 +30,43 @@ module.exports = (SerialPort, dbClient, portDidOpen = noop) => {
     baudRate: 115200,
   })
 
+  let state = 'OPENING'
+
   port.on('open', () => {
+    console.log('open')
+    state = 'OPEN'
     port.write('\r\r', portDidOpen)
   })
 
+  port.on('data', data => {
+    const message = decoder.write(data)
+    switch (state) {
+      case 'OPEN':
+        if (message === '\r\ndwm> ') {
+          state = 'SHELL'
+          port.write('lec\r', () => {
+            state = 'POS'
+            console.log('lec done')
+          })
+        }
+        break
+      case 'POS':
+        console.log(`pos: ${message}`)
+        break
+      default:
+        console.log(`state = ${state}, message = "${message}"`)
+    }
+  })
+
   const parser = port
-    .pipe(new Ready({ delimiter: 'READY' }))
-    .pipe(new Readline({ delimiter: '\n' }))
+    //.pipe(new Ready({ delimiter: 'READY' }))
+    .pipe(new Readline({ delimiter: '\r\n' }))
 
   parser.on('data', data => {
-    console.log('data', data)
-    const coordinates = parseCoordinates(data)
+    console.log(`data: "data"`)
+    //const coordinates = parseCoordinates(data)
 
-    dbClient.saveCoordinates(coordinates)
+    //dbClient.saveCoordinates(coordinates)
   })
 
   return port
